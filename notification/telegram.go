9c9147d6ce01eb1875f12e75930a68bc807223da
罗翀 <r.anerg@gmail.com>
2021-08-26 15:25:45 +0800

    telegram 增加格式化参数

--- notification/telegram.go
+++ notification/telegram.go
@@ -7,15 +7,17 @@ import (
 const apiURI = "https://tgapi.anerg.com/"
 
 type Telegram struct {
-	BotToken string
-	ChatID   string
+	BotToken  string
+	ChatID    string
+	ParseMode string
 }
 
 func (t *Telegram) SendMessage(text string) {
 	uri := apiURI + t.BotToken + "/sendMessage"
 	body := map[string]string{
 		"chat_id": t.ChatID,
-		"text":    text,
+		"text": text,
+		"parse_mode": t.ParseMode
 	}
 	resty.HttpClient.R().SetBody(body).Post(uri)
 }
